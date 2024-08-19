e8821f955ba229fef5e179aea18d3b2ef6826fde
kb7-639 <mms.awrb@gmail.com>
2024-08-19 14:38:44 +0900

    不要な記述の削除

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -8,26 +8,22 @@ public class GameController : MonoBehaviour
     public GameObject BossMonster;
     //外部のクラスからアクセス可能な変数 ... GameController.EnemyCounter
     static public int EnemyCounter;
-    bool test = true;
+
     // Start is called before the first frame update
     void Start()
     {
         //初期化
         EnemyCounter = 2;
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
         //キューブの生成
         if (EnemyCounter == 0)
         {
             Debug.Log("敵のカウンターが０になった");
             GameObject go = Instantiate(BossMonster);
-
         }
-        
     }
 }
